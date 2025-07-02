07ac2c75ef1af492c9eaf092c72cc49c6123198d
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-07-02 14:49:06 -0300

    feat: arparse mt bao

---
+++ arg_parse.py
@@ -0,0 +1,25 @@
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument("-b", "--basic",
+                    help="Mostra "Olá mundo" na tela",
+                    type=str, #tipo do argumento
+                    metavar="STRING", 
+                    #default="Olá Mundo", #valor padrão
+                    required=False,
+                    #nargs="+" #recebe mais de um valor
+                    action="append")
+parser.add_argument("-v", "--verbose",
+                    help="Mostra logs",
+                    type=str, #tipo do argumento
+                    metavar="STRING", 
+                    #default="Olá Mundo", #valor padrão
+                    required=False,
+                    #nargs="+" #recebe mais de um valor
+                    action="store_true")
+args = parser.parse_args()
+
+if args.basic is None:
+    print("Você não passou o valor de b/basic")
+else:
+    print("O valor de basic é: ", args.basic)
